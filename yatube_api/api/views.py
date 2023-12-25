7c6239627f883df2b8103cd7f16ccbc3dd707ec1
Олег Говоровский <oleq.gov@ya.ru>
2023-12-25 22:22:38 +0500

    add djoser

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,18 @@
 # TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Post
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    pagination_class = (LimitOffsetPagination,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
